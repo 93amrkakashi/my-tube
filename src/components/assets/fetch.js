65e0055bdd32f1dfed922d1dbbe22c79dd7edec9
Amr Ali <110984363+93amrkakashi@users.noreply.github.com>
2023-02-06 23:09:16 +0200

    Delete fetch.js

--- src/components/assets/fetch.js
+++
@@ -1,9 +0,0 @@
-
-
-export const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'e1a35da017mshe54f24e83be68a9p1543e2jsn711a0b317ba4',
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-  }
-};
